0498055ce5037d9c1c27dc3d8ac578972d8bf835
Thepuppetqueen57 <doggyandkira2013@gmail.com>
2025-11-04 17:22:10 -0800

    Ui improvements
    Its in a tab container now because I plan on adding a mines minigame
    like in rugplay

--- Scripts/FlipCoin.cs
+++ Scripts/FlipCoin.cs
@@ -3,7 +3,7 @@ using System;
 
 public partial class FlipCoin : Button {
     private void _on_button_up() {
-        int BetAmount = int.Parse(GetNode<LineEdit>("/root/Root/CenterContainer/Control/CoinFlipContainer/BetAmountTextbox").Text);
+        int BetAmount = int.Parse(GetNode<LineEdit>("/root/Gambling/Minigames/BetAmountTextbox").Text);
 
         int WinOrLose = new Random().Next(0, 2);
 
